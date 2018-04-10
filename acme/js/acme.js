435ac58f7b7b3e5e3d8cfd633919c67c926c69b5
knobes25 <35538905+knobes25@users.noreply.github.com>
2018-04-09 18:03:40 -0600

    acme final project

--- acme/js/acme.js
+++ acme/js/acme.js
@@ -11,67 +11,66 @@ const path = "/acme/js/acme.json";
     .catch(error =>console.log('There was an error: ', error));
 }
 getJson();
+
+
 //Function for the nav bar
-let nav = document.getElementById("page_nav");
-function navBar(data){
-    let navarray = ["Anvils", "Explosives", "Decoys", "Traps"];
-    let page_nav = document.getElementById("page_nav")
-    
-    let list = "<ul>";
-    for (let i=0, n = 5; i < n; i++) {
-        list += "<li>" + data[navarray[i]] + "</li>";
-    };
-    list =+ "<ul>";
-    
-    page_nav.innerHTML = list;
-    
-    let anvil= document.getElementById("Anvils");
-    let explosives = document.getElementById("Explosives");
-    let decoys = document.getElementById("Decoys");
-    let traps = document.getElementById("Traps");
-    
-    anvil.addEventListener("click", function(){
-        displayData(data, navarray, 0)
-    });
-    explosives.addEventListener("click", function(){
-        displayData(data, navarray, 1)
-    });
-    decoys.addEventListener("click", function(){
-        displayData(data, navarray, 2)
-    });
-    traps.addEventListener("click", function(){
-        displayData(data, navarray, 3)
-    });
+function navBar(data) {
+   for (i=0; i<=5; i++){
+       let navname = data.navarray[i];
+       let display = '';
+       display += navname;
+       console.log(display);
+       document.getElementById(i).innerHTML = display;
+   }
+   
+       
+ 
 }
 
-function displayData(data, name, n) {
+let nav = document.getElementById("page_nav");
+nav.addEventListener("click", function(event) {
+    let selection = event.target.innerHTML;
+    event.preventDefault();
+    console.log(selection);
+    if(selection !== 'Home'){
+        document.getElementById('home-page').setAttribute('class', 'hide');
+        document.getElementById('product_container').setAttribute('class', 'show');
+        const path = "/acme/js/acme.json";
+        fetch(path)
+        .then(response => response.json())
+        .then(function(data){
+        console.log('json object');
+        console.log(data);
+        data = data[selection];
+        displayData(data);
+    })
+    .catch(error =>console.log('There was an error: ', error));
+}
+    
+    else if(selection == 'Home'){
+         document.getElementById('home-page').setAttribute('class', 'show');
+        document.getElementById('product_container').setAttribute('class', 'hide');
+        document.getElementById("title").innerHTML = "ACME | Home"; 
+    }
+    
+});
+
+function displayData(data) {
      let home = document.getElementById("home-page");
     let name = document.getElementById("name");
-    let info = document.getElementById("information");
     let pic = document.getElementById("productImage");
     let description = document.getElementById("description");
     let maker = document.getElementById("manufacturer");
     let reviews = document.getElementById("reviews");
     let price = document.getElementById("price");
-
-
-    let product_container = document.getElementById("product_container");
-    product_container.setAttribute("class", "show");
-    home.setAttribute("class", "hide")
     
-    name.innerHTML = data[name[n]].name;
-    pic.innerHTML = "<img src = '" + data[name[n]].path + "' alt= 'image of current product' id='productImage'/>"
-    description.innerHTML = data[name[n]].description;
-    maker.innerHTML = data[name[n]].manufacturer;
-    reviews.innerHTML = data[name[n]].reviews;
-    price.innerHTML = data[name[n]].price;
+    name.innerHTML = data.name;
+    pic.src = data.path;
+    description.innerHTML = data.description;
+    maker.innerHTML = data.manufacturer;
+    reviews.innerHTML = data.reviews;
+    price.innerHTML = data.price;
+    document.getElementById("title").innerHTML = "ACME | " + data.name;
 }
 
 
-//let nav = document.getElementById("page-nav");
-//nav.addEventListener("click", function(event) {
-//    event.preventDefault();
-//    let product = event.target.dataset.name;
-//    console.log(product);
-//    displayData(data, name, n); 
-//})
